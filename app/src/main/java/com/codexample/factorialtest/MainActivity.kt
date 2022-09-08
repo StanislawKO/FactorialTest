c93ccda54773036fdf6b71cbe9e2863fe942c255
Stanislav Kozh <printks@yandex.ru>
2022-09-08 21:38:49 +0300

    Create Stateful View Model

--- app/src/main/java/com/codexample/factorialtest/MainActivity.kt
+++ app/src/main/java/com/codexample/factorialtest/MainActivity.kt
@@ -2,10 +2,43 @@ package com.codexample.factorialtest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.lifecycle.ViewModelProvider
+import com.codexample.factorialtest.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    private val viewModel by lazy {
+        ViewModelProvider(this)[MainViewModel::class.java]
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+        observeViewModel()
+        binding.buttonCalculate.setOnClickListener {
+            viewModel.calculate(binding.editTextNumber.text.toString())
+        }
+    }
+
+    private fun observeViewModel() {
+        viewModel.state.observe(this) {
+            if (it.isError) {
+                Toast.makeText(this, "You did not entered value", Toast.LENGTH_SHORT).show()
+            }
+            if (it.isInProgress) {
+                binding.progressBarLoading.visibility = View.VISIBLE
+                binding.buttonCalculate.isEnabled = false
+            } else {
+                binding.progressBarLoading.visibility = View.GONE
+                binding.buttonCalculate.isEnabled = true
+            }
+            binding.textViewFactorial.text = it.factorial
+        }
     }
 }
---
+++ app/src/main/java/com/codexample/factorialtest/MainViewModel.kt
@@ -0,0 +1,29 @@
+package com.codexample.factorialtest
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+class MainViewModel : ViewModel() {
+
+    private val _state = MutableLiveData<State>()
+    val state: LiveData<State>
+        get() = _state
+
+    fun calculate(value: String?) {
+        _state.value = State(isInProgress = true)
+        if (value.isNullOrBlank()) {
+            _state.value = State(isError = true)
+            return
+        }
+        viewModelScope.launch {
+            val number = value.toLong()
+            //calculate
+            delay(1000)
+            _state.value = State(factorial = number.toString())
+        }
+    }
+}
